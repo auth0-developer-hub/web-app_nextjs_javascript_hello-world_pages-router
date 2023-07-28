e2aba89f45e0832c048404b413a792ebd0b7674b
Byron Motoche <116190812+byron-okta@users.noreply.github.com>
2023-07-28 17:07:54 -0500

    implement authentication with external api integration

--- pages/api/messages/protected.js
+++ pages/api/messages/protected.js
@@ -1,11 +1,36 @@
-import { withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { callExternalApi } from "../../../services/external-api.service";
+
+const apiServerUrl = process.env.API_SERVER_URL;
 
 const getProtectedMessage = async (req, res) => {
-  const message = {
-    text: "This is a protected message.",
-  };
+  try {
+    const { accessToken } = await getAccessToken(req, res);
 
-  res.status(200).json(message);
+    const config = {
+      url: `${apiServerUrl}/api/messages/protected`,
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+
+    const { data, error, status } = await callExternalApi({
+      config,
+    });
+
+    if (data) {
+      res.status(status).json(data);
+      return;
+    }
+
+    res
+      .status(status || 500)
+      .json(error || { message: "Unable to retrieve message" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 export default withApiAuthRequired(getProtectedMessage);
